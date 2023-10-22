0399295446bcba756887946da4ff04c5ad536b23
Larisa-Blegu <blegu.larisa@gmail.com>
2023-10-22 13:56:43 +0300

    Add project files.

---
+++ Data/Blegu_Larisa_lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Blegu_Larisa_lab2.Models;
+
+namespace Blegu_Larisa_lab2.Data
+{
+    public class Blegu_Larisa_lab2Context : DbContext
+    {
+        public Blegu_Larisa_lab2Context (DbContextOptions<Blegu_Larisa_lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Blegu_Larisa_lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Blegu_Larisa_lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Blegu_Larisa_lab2.Data;
+using Blegu_Larisa_lab2.Models;
+
+namespace Blegu_Larisa_lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Blegu_Larisa_lab2.Data.Blegu_Larisa_lab2Context _context;
+
+        public DetailsModel(Blegu_Larisa_lab2.Data.Blegu_Larisa_lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Book Book { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
